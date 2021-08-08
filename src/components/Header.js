e2a25c29b8044b6978790aec00ea1165a2667f5f
coreyo-git <corey.oconnor@live.com.au>
2021-08-09 09:22:04 +1000

    Added PropTypes for stricter variable assignemts on props

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,16 @@
+import PropTypes from 'prop-types'
+
+
 const Header = (props) => {
     return (
         <header>
             <h1>{props.title}</h1>            
         </header>
     )
+}
+// Typing for props
+Header.PropTypes = {
+    title: PropTypes.string.isRequired,
 }
 
 Header.defaultProps = {
