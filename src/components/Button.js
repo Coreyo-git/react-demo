4f2135dca6cf4702af8b628ae0769f177cab07f5
coreyo-git <corey.oconnor@live.com.au>
2021-08-16 07:16:12 +1000

    Added onlick button function and passed it to header.js

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,16 @@
-const Button = ({color, text}) => {
-    return <button style={{backgroundColor: color}} className='btn'>{text}</button>
+const Button = ({onClick, color, text}) => {
+    return <button onClick={onClick}  style={{backgroundColor: color}} className='btn'>{text}</button>
 }
 
 Button.defaultProps = {
     color: 'green',
     text: 'Default',
+}
+
+Button.propTypes = {
+    color: PropTypes.string,
+    text: PropTypes.string,
+    onClick: PropTypes.func
 }
 
 export default Button
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,13 @@ import Button from './Button';
 
 
     const Header = ({ title }) => {
+        const onClick = () => {
+            console.log('Clicked');
+        }
     return (
         <header className='header'>
             <h1>{title}</h1>
-            <Button color='black' text='Add'/>
+            <Button color='green' text='Add' onClick={onClick}/>
         </header>
     )
 }
