5205719028aa6fbfdf5b03c9d56d961cd2085793
coreyo-git <corey.oconnor@live.com.au>
2021-08-16 05:23:50 +1000

    Added TextField Component and it doesn't spin up now?

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/Header';
 import JobList from './components/JobList';
+import TextField from './components/TextField';
 
 function App() {
   return (
     <div className="container">
       <Header />
       <JobList />
+      <TextField />
     </div>
   );
 }
